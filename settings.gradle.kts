92e94ddb08495b55fa58020289c9c23767628558
Sergey Okatov <sokatov@gmail.com>
2024-11-30 22:09:40 +0500

    m1l1 Stream Fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "otuskotlin-marketplace-202408"
+rootProject.name = "otuskotlin-marketplace-202411"
 
 include("m1l1-first")
 
