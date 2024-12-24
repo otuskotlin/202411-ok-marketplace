7e9a71f0213a7704c90badf1b4dc60950be4796a
sproshchaev <sproshchaev@gmail.com>
2024-12-24 13:45:09 +0500

    add coroutines-core

--- m2l3-flows/build.gradle.kts
+++ m2l3-flows/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.slf4j:slf4j-api:2.0.9")
     implementation("ch.qos.logback:logback-classic:1.4.11")
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
 
     testImplementation(kotlin("test-junit"))
 }
