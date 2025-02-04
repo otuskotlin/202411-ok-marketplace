57901d7cb657c66b269d932990f917bac5147b10
sproshchaev <sproshchaev@gmail.com>
2025-02-04 14:56:10 +0500

    add mappers, common

---
+++ ok-marketplace-be/ok-marketplace-common/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("build-kmp")
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+kotlin {
+    sourceSets {
+        val coroutinesVersion: String by project
+        commonMain {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+                implementation(libs.kotlinx.serialization.core)
+                implementation(libs.kotlinx.serialization.json)
+
+                // transport models
+                implementation(project(":ok-marketplace-common"))
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+                implementation(project(":ok-marketplace-api-v2-kmp"))
+            }
+        }
+
+        jvmTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        nativeTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
