f8a38889d67022eddc178c6d096addcc0d28114a
evgnep <enji81@gmail.com>
2025-03-30 20:31:44 +0300

    M7l2 repo (#32)
    Co-authored-by: Sergey Okatov <svok@users.noreply.github.com>

--- ok-marketplace-be/ok-marketplace-api-v1-jackson/src/main/kotlin/ApiV1Mapper.kt
+++ ok-marketplace-be/ok-marketplace-api-v1-jackson/src/main/kotlin/ApiV1Mapper.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.json.JsonMapper
 import ru.otus.otuskotlin.marketplace.api.v1.models.IRequest
 import ru.otus.otuskotlin.marketplace.api.v1.models.IResponse
+
 val apiV1Mapper = JsonMapper.builder().run {
 //    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     enable(MapperFeature.USE_BASE_TYPE_AS_DEFAULT_IMPL)
